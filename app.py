47ec8bdd84cf2741ea366a91a8bbac3251e3a74e
lbv_o <gromlbv@gmail.com>
2025-10-28 15:33:31 +0300

    bot to other py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, flash, request
-from telegramto import send_tg, start_bot
-import threading
+from telegramto import send_tg
 
 app = Flask(__name__)
 
@@ -62,11 +61,4 @@ def send():
 
 
 if __name__ == "__main__":
-    print('Starting bot...')
-    try:
-        bot_thread = threading.Thread(target=start_bot, daemon=True)
-        bot_thread.start()
-    except Exception as e:
-        print(f"error {e}")
-    print('Bot started')
     app.run(debug=True, port='5400', use_reloader=False)
